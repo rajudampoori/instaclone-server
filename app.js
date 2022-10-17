f152747dd0b9bd1ca6b6ceba4ba78e3634623f8b
raju <rajudampoori@gmail.com>
2022-10-17 22:22:02 +0530

    eleventh commit

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const mongoose = require("mongoose")
 const app = express()
 const route = require("./src/routes/routes")
 const cors = require("cors")
-const port = process.env.port || 8000
+const port = process.env.PORT || 8000
 
 app.use(cors())
 app.use("/",route)
 app.use(express.static("public"))
 mongoose.connect("mongodb+srv://root:rootroot@cluster0.mgswfkd.mongodb.net/?retryWrites=true&w=majority"
 ,()=> {console.log("Connected to DB")})
-
+app.get("*",(req,res)=> {
+    res.send("page not found")
+})
 app.listen(port,()=> { console.log(`Server is up at ${port}`)})
 //mongodb+srv://root:root@cluster0.mgswfkd.mongodb.net/?retryWrites=true&w=majority
