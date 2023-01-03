3720f867ed96035d82fa695fdad717e0350c4ae4
raju <rajudampoori@gmail.com>
2023-01-03 20:19:35 +0530

    eighth commit

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const port = process.env.PORT || 8000
 app.use(cors())
 app.use("/",route)
 app.use(express.static("public"))
-mongoose.connect("mongodb+srv://root:root@cluster0.mc6f43m.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect("mongodb+srv://root:root@cluster0.r8newh2.mongodb.net/?retryWrites=true&w=majority"
+//mongodb+srv://root:root@cluster0.r8newh2.mongodb.net/?retryWrites=true&w=majority
 ,()=> {console.log("Connected to DB")})
 app.get("*",(req,res)=> {
     res.send("page not found")
